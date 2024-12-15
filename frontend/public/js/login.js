167484ed622e202d1b5f9d3bb4239de0cdb3ea0a
유건주 <gunjudavid@naver.com>
2024-12-15 17:27:21 +0900

    fix: cors for new deploy

--- frontend/public/js/login.js
+++ frontend/public/js/login.js
@@ -110,7 +110,7 @@ class LoginForm {
             try {
                 const response = await fetch(
                     //'http://localhost:3000/auth/login',
-                    'http://43.203.237.161/api/auth/login',
+                    'http://kakao-community-docker-env.eba-fmgi3uwj.ap-northeast-2.elasticbeanstalk.com/api/auth/login',
                     {
                         method: 'POST',
                         headers: { 'Content-Type': 'application/json' },
